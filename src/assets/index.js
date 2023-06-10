ca1577817a3ae366a755873b375b5858acb2e85d
suyashgrewal24 <suyashgrewal32@gmail.com>
2023-06-10 15:08:33 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import loader from './loader.svg';
+import logo from './logo.svg';
+import myPic from './myPic.png'
+import withBg from './with-bg.png'
+import withoutBg from './without-bg.png'
+
+
+export { logo, withBg, withoutBg, loader, myPic };
---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,25 @@
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+import React from 'react';
+
+const PlayPause = ({
+  isPlaying,
+  activeSong,
+  song,
+  handlePause,
+  handlePlay,
+}) => (
+  <div>
+    {isPlaying && activeSong?.title === song.title ? (
+      <FaPauseCircle
+        size={35}
+        className="text-gray-300"
+        onClick={handlePause}
+      />
+    ) : (
+      <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
+    )}
+  </div>
+);
+
+export default PlayPause;
